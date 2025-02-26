5ef91c09e9e3f8abd02bbc7f13a2db47a6740655
Osvaldo Mesa <d3v.ozzy@gmail.com>
2025-02-25 20:01:22 -0800

    align input with button on home page
    update css to include global box-sizing to border box.
    It forces elements on the page to respect the space of other
    elements.

--- src/atoms/store.ts
+++ src/atoms/store.ts
@@ -1,6 +1,6 @@
-import {atom} from "jotai/vanilla";
-import {PromptAndResponse} from "../types/PromptAndResponse";
+import { atom } from "jotai/vanilla";
+import { PromptAndResponse } from "../types/PromptAndResponse";
 
 export const messagesAtom = atom(new Array<PromptAndResponse>());
 
-export const stompAtom = atom({connected: false});
+export const stompAtom = atom({ connected: false });
--- src/chat/Pages2.tsx
+++ src/chat/Pages2.tsx
@@ -3,71 +3,71 @@ import React, { useEffect, useState } from "react";
 //@ts-nocheck
 import { socket } from "../listeners/socket"
 import "./index.css";
-import {PromptAndResponse} from "../types/PromptAndResponse";
-import {useAtom} from "jotai";
-import {messagesAtom} from "../atoms/store";
+import { PromptAndResponse } from "../types/PromptAndResponse";
+import { useAtom } from "jotai";
+import { messagesAtom } from "../atoms/store";
 
 function Page2() {
-    //Room State
-    const [room, setRoom] = useState("");
+  //Room State
+  const [room, setRoom] = useState("");
 
-    // Messages States
-    const [message, setMessage] = useState("");
-    const [messageReceived, setMessageReceived] = useAtom(messagesAtom);
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useAtom(messagesAtom);
 
-    const joinRoom = () => {
-        if (room !== "") {
-            socket.emit("chat", room);
-        }
-    };
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("chat", room);
+    }
+  };
 
 
-    const sendMessage = () => {
-        // joinRoom();
-        socket.emit("send_message", { message, room });
-    };
+  const sendMessage = () => {
+    // joinRoom();
+    socket.emit("send_message", { message, room });
+  };
 
-    useEffect(() => {
-        socket.on("receive_message", (data: any) => {
-            const message = data && data.message ? data.message : data;
-            setMessageReceived(prevMessages => [...prevMessages, message]);
-        });
-    }, []);
-        return (
-            <div className="App">
-                <h1>Message:</h1>
-                <div>
-                    {messageReceived.map((promptAndResponse, index) => (
-                        <div key={index} className="message-container">
-                            <p>{promptAndResponse.prompt}</p>
-                            <br/>
-                            <p>{promptAndResponse.response}</p>
-                        </div>
-                    ))}
-                </div>
-                <input
-                    type="text"
-                    placeholder="Room Number..."
-                    onChange={(event) => {
-                        setRoom(event.target.value);
-                    }}
-                />
-                <button onClick={joinRoom}>Join Room</button>
-                <input
-                    type="text"
-                    placeholder="Message..."
-                    onChange={(event) => {
-                        setMessage(event.target.value);
-                    }}
-                    onKeyPress={(event) => {
-                        if (event.key === 'Enter') {
-                            sendMessage();
-                        }
-                    }}
-                />
-                <button onClick={sendMessage}>Send Message</button>
-            </div>
-        );
+  useEffect(() => {
+    socket.on("receive_message", (data: any) => {
+      const message = data && data.message ? data.message : data;
+      setMessageReceived(prevMessages => [...prevMessages, message]);
+    });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Message:</h1>
+      <div>
+        {messageReceived.map((promptAndResponse, index) => (
+          <div key={index} className="message-container">
+            <p>{promptAndResponse.prompt}</p>
+            <br />
+            <p>{promptAndResponse.response}</p>
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join Room</button>
+      <input
+        type="text"
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            sendMessage();
+          }
+        }}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
+  );
 
 }
 
